2b03ede486c11fab3e513ab6bf25f42f26617d62
jsk <jsklebes@googlemail.com>
2023-04-25 08:30:52 +0100

    doc: describe question, try doctest

--- python/problem_2.py
+++ python/problem_2.py
@@ -2,20 +2,27 @@
 
 import sys
 
-def solution(n):
+def sumEvenFibonacci(n):
+    """ What is the sum of all even Fibonnaci numbers less than n?
+    >>> 10
+    10
+    >>> 60
+    44
+    """
     prev_x=0
     x = 2
     sum_ = 0
     while x < n:
         sum_+=x
-        # the next even fibonacci number is generated from 2 
+        # the next even fibonacci number is generated from two
         # previous even fibonacci numbers as a_{i+1}= 4*a_i + a_{i-1}
         new_x = x*4+prev_x
         prev_x = x
         x = new_x
     return sum_
 
-t = int(input().strip())
-for a0 in range(t):
-    n = int(input().strip())
-    print(solution(n))
+if __name__=="__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        n = int(input().strip())
+        print(sumEvenFibonnaci(n))

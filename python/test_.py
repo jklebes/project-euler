30554f7433570531aa5ce83ef5f6b72297556ce8
jsk <jsklebes@googlemail.com>
2023-04-25 18:07:55 +0100

    testing the right directory?

--- python/test_.py
+++ python/test_.py
@@ -2,6 +2,7 @@ import doctest
 import glob
 
 def test_doctestall(): 
-    files = glob.glob("*.py")
+    files = glob.glob("python/*.py")
+
     for f in files:
         assert doctest.testfile(f, verbose=True)[0]==0

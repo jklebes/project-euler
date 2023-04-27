a6877a30f0c1dbd8271c23f552cd8500f29e04f0
jsk <jsklebes@googlemail.com>
2023-04-27 19:56:19 +0100

    pep8, some tests

--- python/test_.py
+++ python/test_.py
@@ -1,11 +1,11 @@
 def test_problem_2():
-    import python.problem_2 as p2
+    import problem_2 as p2
     assert p2.sumEvenFibonacci(10) == 10
     assert p2.sumEvenFibonacci(100) == 44
 
 
 def test_problem_3():
-    import python.problem_3 as p3
+    import problem_3 as p3
     assert p3.largest_prime_factor(13195) == 29
     # special case i^m, here 5^4
     x = 5**4
@@ -15,12 +15,12 @@ def test_problem_3():
 
 
 def test_problem_5():
-    import python.problem_5 as p5
+    import problem_5 as p5
     assert p5.multi_lcm(10) == 2520
 
 
 def test_problem_6():
-    import python.problem_6 as p6
+    import problem_6 as p6
     assert p6.squarediff(10) == 2640
 
 

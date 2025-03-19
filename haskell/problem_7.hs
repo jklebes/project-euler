b5146110cdb09ccd1a51556c6f4680135438c614
jklebes <jason.klebes@ed.ac.uk>
2025-03-19 11:17:35 +0000

    Restored problem 7 after mixup with problem 9

--- haskell/problem_7.hs
+++ haskell/problem_7.hs
@@ -1,36 +1,18 @@
 import Control.Applicative
 import Control.Monad
 import System.IO
-import Data.List
 
-main :: IO ()
 main = do
     t_temp <- getLine
     let t = read t_temp :: Int
-    forM_ [1..t] $ \a0  -> do
-        n_temp <- getLine
-        let n = read n_temp :: Int
-        putStrLn $ show $ ans n
+    ns_temp <- getContents
+    let ns = map (read::String->Int) $ words ns_temp
+    let answers = map (\y -> primes  !! (y-1)) ns
+    mapM (putStrLn . show) $ answers
 
-tripleSum :: Int -> Int -> Int
-tripleSum p q = 2*p^2 + 2*p*q
+--primes :: [Int]
+primes = 2: [x | x <- [3..], isprime x]
+isprime x = all (\p -> x `mod` p >0) (listUpToSqrt x)
+  where
+    listUpToSqrt x = takeWhile (\p -> p*p <= x) primes
 
-tripleProd :: Int -> (Int, Int) -> Int
-tripleProd n (p,q) = (p^2 - q^2)*(2*q*p)*(p^2 + q^2)*r^3
-  where r = n `div` (tripleSum p q)
-
-ans :: Int -> Int
-ans n = mymaximum (map (tripleProd n) (filter (\t -> checkpq t n) (psqsToCheck n)))
-
-mymaximum [] = -1
-mymaximum xs = maximum xs
-
-psqsToCheck :: Int -> [(Int, Int)]
-psqsToCheck n = [(p,q) | p <- psToCheck n, q <- qsToCheck p n]
-
-psToCheck n = takeWhile (\x -> 2*x^2 <= n ) [1..]
-
-qsToCheck p n = takeWhile (\x -> (2*p^2 + 2*x*p) <= n ) [1..(p-1)]
-
-checkpq :: (Int,Int) -> Int -> Bool
-checkpq (p,q) n = n `mod` (tripleSum p q) == 0
